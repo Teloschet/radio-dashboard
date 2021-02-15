09ee9ac2b501848a6ff39b1838001208e2a405dc
Teloschet <frapgameroficial@hotmail.com>
2021-02-15 19:19:22 -0300

    new style attualization

--- index.php
+++ index.php
@@ -12,13 +12,34 @@
     <link rel="stylesheet" type="text/css" href="assets/global.css">
 
     </head>
-<body>
-    <header>
-        <div class="logo">dashborad</div>
-    </header>
-
-    <div id="tudo">
-
+<body onload="startTime()">
+<div class="wrapper">
+    <div class="sidebar">
+        <h2>DASHBOARD</h2>
+        <ul>
+            <li><a href="#"><i class="fas fa-globe"></i>Home</a></li>
+            <li><a href="#"><i class="fas fa-microphone"></i>On Air</a></li>
+            <li><a href="#"><i class="fas fa-newspaper"></i>News</a></li>
+            <li><a href="#"><i class="fas fa-clock"></i>Schedules</a></li>
+        </ul> 
+        <div class="social_media">
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+      </div>
     </div>
+    <div class="main_content">
+        <div class="header">Welcome to Radio Panel <div id="txt" style="float:right;"></div></div>  
+        <div class="info">
+          <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A sed nobis ut exercitationem atque accusamus sit natus officiis totam blanditiis at eum nemo, nulla et quae eius culpa eveniet voluptatibus repellat illum tenetur, facilis porro. Quae fuga odio perferendis itaque alias sint, beatae non maiores magnam ad, veniam tenetur atque ea exercitationem earum eveniet totam ipsam magni tempora aliquid ullam possimus? Tempora nobis facere porro, praesentium magnam provident accusamus temporibus! Repellendus harum veritatis itaque molestias repudiandae ea corporis maiores non obcaecati libero, unde ipsum consequuntur aut consectetur culpa magni omnis vero odio suscipit vitae dolor quod dignissimos perferendis eos? Consequuntur!</div>
+          <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A sed nobis ut exercitationem atque accusamus sit natus officiis totam blanditiis at eum nemo, nulla et quae eius culpa eveniet voluptatibus repellat illum tenetur, facilis porro. Quae fuga odio perferendis itaque alias sint, beatae non maiores magnam ad, veniam tenetur atque ea exercitationem earum eveniet totam ipsam magni tempora aliquid ullam possimus? Tempora nobis facere porro, praesentium magnam provident accusamus temporibus! Repellendus harum veritatis itaque molestias repudiandae ea corporis maiores non obcaecati libero, unde ipsum consequuntur aut consectetur culpa magni omnis vero odio suscipit vitae dolor quod dignissimos perferendis eos? Consequuntur!</div>
+          <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A sed nobis ut exercitationem atque accusamus sit natus officiis totam blanditiis at eum nemo, nulla et quae eius culpa eveniet voluptatibus repellat illum tenetur, facilis porro. Quae fuga odio perferendis itaque alias sint, beatae non maiores magnam ad, veniam tenetur atque ea exercitationem earum eveniet totam ipsam magni tempora aliquid ullam possimus? Tempora nobis facere porro, praesentium magnam provident accusamus temporibus! Repellendus harum veritatis itaque molestias repudiandae ea corporis maiores non obcaecati libero, unde ipsum consequuntur aut consectetur culpa magni omnis vero odio suscipit vitae dolor quod dignissimos perferendis eos? Consequuntur!</div>
+      </div>
+    </div>
+</div>
+
+<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+<script src="assets/clock.js"></script>
+
 </body>
 </html>
